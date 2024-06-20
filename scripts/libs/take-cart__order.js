08c2c261033abd29b05bcecf660e5568b0775270
Nobuyuki Takahiro <vivlio@kumihan.com>
2024-06-20 22:52:55 +0900

    create order _init func

--- scripts/libs/take-cart__order.js
+++ scripts/libs/take-cart__order.js
@@ -10,51 +10,58 @@ class Order {
     this._init()
   }
 
+
   _init() {
     this.confirmOrderBtn.addEventListener("click", () => {
-      const getOrderlistEl = () => {
-        // orderedEachItemResult()から配列を取得する。
-        const orderItems = this.calcIns.orderedEachItemResult();
-        // reduce()を使用してリストアイテムのHTMLを生成する。
-        const liContent = orderItems.reduce((acc, obj) => {
-          // 個数はオブジェクトなので、文字列に変換するため分けて処理する。
-          const orderQuantity = Object.keys(obj["内訳"]).reduce((acc, key) => {
-            const list = key !== "SELF"
-              ? `<span>${key}／${obj["内訳"][key]}個</span>`
-              : `<span>${obj["内訳"][key]}個</span>`;
-            return acc + list;
-          }, "");
-          // li要素の生成。
-          const itemLiContent = `
-            <li data-order-item-name=${obj["品名"]}>
-              <div class="name">品名 : ${obj["品名"]}</div>
-              <div class="quantity">数量 : ${orderQuantity}</div>
-              <div class="sub-total">小計 : ${obj["小計"]}円</div>
-            </li>
-          `;
-          // accumulatorに現在のアイテムを追加して統合していく。
-          return acc + itemLiContent;
-        }, "");
-        return liContent;
-      };
-    
-      // 既存のリストをクリア
-      this.orderResultEl.innerHTML = '';      
-    
-      // 生成されたHTML文字列をDOM要素に変換
-      const tempDiv = document.createElement("div");
-      tempDiv.innerHTML = getOrderlistEl();
-      Array.from(tempDiv.children).forEach(el => {
-        this.orderResultEl.prepend(el);
-      })
+      if (this.calcIns.cart.length !== 0) {
+        // 注文をリストにして返す。
+        const orderLiHTML = this.getOrderlistEl();
+      
+        // 既存のリストをクリア
+        this.orderResultEl.innerHTML = '';      
+      
+        // 生成されたHTML文字列をDOM要素に変換
+        const tempDiv = document.createElement("div");
+        tempDiv.innerHTML = orderLiHTML;
+        Array.from(tempDiv.children).forEach(el => {
+          this.orderResultEl.prepend(el);
+        })
+      } else {
+        console.log("nothing");
+      }
     });
-    
-    
+
     // 戻るボタンのイベントリスナー。
     this.backToCartBtn.addEventListener("click", () => {
       this.orderResultEl.innerHTML = '';
-    });
+    });    
   }
+
+  getOrderlistEl() {
+    // orderedEachItemResult()から配列を取得する。
+    const orderItems = this.calcIns.orderedEachItemResult();
+    // reduce()を使用してリストアイテムのHTMLを生成する。
+    const liContent = orderItems.reduce((acc, obj) => {
+      // 個数はオブジェクトなので、文字列に変換するため分けて処理する。
+      const orderQuantity = Object.keys(obj["内訳"]).reduce((acc, key) => {
+        const list = key !== "SELF"
+          ? `<span>${key}／${obj["内訳"][key]}個</span>`
+          : `<span>${obj["内訳"][key]}個</span>`;
+        return acc + list;
+      }, "");
+      // li要素の生成。
+      const itemLiContent = `
+        <li data-order-item-name=${obj["品名"]}>
+          <div class="name">品名 : ${obj["品名"]}</div>
+          <div class="quantity">数量 : ${orderQuantity}</div>
+          <div class="sub-total">小計 : ${obj["小計"]}円</div>
+        </li>
+      `;
+      // accumulatorに現在のアイテムを追加して統合していく。
+      return acc + itemLiContent;
+    }, "");
+    return liContent;
+  };
 }
 
 
